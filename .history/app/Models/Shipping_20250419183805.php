a9950435d3a7f6b6092023f13be47ad5730fbf25
Lam Kha <builamkha004@gmail.com>
2025-04-20 20:16:58 +0700

    update

---
+++ .history/app/Models/Shipping_20250419183805.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shipping extends Model
+{
+    //
+    use HasFactory;
+    protected $fillable = ['name','phone','address','email'];
+
+    public function products(){
+        return $this->hasMany(related: Product::class);
+    }
+
+}
